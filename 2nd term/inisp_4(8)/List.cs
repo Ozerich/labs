1f9e531ad95b9487c6f64ab79daaa0ae4eee5d2e
Ozerich <ozicoder@gmail.com>
2011-05-15 01:56:34 +0300

    Added inisp_2_4(8)

---
+++ 2nd term/inisp_4(8)/List.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+public class ListItem
+{
+    public ListItem Next{ get; set; }
+    public object Data { get; set; }
+
+    public ListItem(object val)
+    {
+        Data = val;
+    }
+}
+
+
+abstract public class List
+{
+    protected ListItem beg, end;
+
+    public void AddBack(object item)
+    {
+        ListItem NewNode = new ListItem(item);
+        if (beg == null)
+            beg = end = NewNode;
+        else
+        {
+            end.Next = NewNode;
+            end = NewNode;
+        }
+    }
+
+    public void AddFront(object item)
+    {
+        ListItem NewNode = new ListItem(item);
+        if (beg == null)
+            beg = end = NewNode;
+        else
+        {
+            NewNode.Next = beg;
+            beg = NewNode;
+        }
+    }
+
+    public object GetFront()
+    {
+        return beg.Data;
+    }
+
+    public object GetBack()
+    {
+        return end.Data;
+    }
+
+
+    public void Print()
+    {
+        ListItem CurNode = beg;
+        while (CurNode != null)
+        {
+            Console.Write(CurNode.Data + " ");
+            CurNode = CurNode.Next;
+        }
+    }
+
+}
+
+
+class IntList : List
+{
+    public void AddBack(int item)
+    {
+        base.AddBack(item);
+    }
+
+    public void AddFront(int item)
+    {
+        base.AddFront(item);
+    }
+
+    public int GetFront()
+    {
+        return (int)base.GetFront();
+    }
+
+    public int GetBack()
+    {
+        return (int)base.GetBack();
+    }
+
+    public bool Find(int val)
+    {
+        ListItem CurNode = beg;
+        while(CurNode != null)
+        {
+            if((int)CurNode.Data == val)
+                return true;
+            CurNode = CurNode.Next;
+        }
+        return false;
+    }
+}
+
+
+class StringList : List
+{
+    public void AddBack(string item)
+    {
+        base.AddBack(item);
+    }
+
+    public void AddFront(string item)
+    {
+        base.AddFront(item);
+    }
+
+    public string GetFront()
+    {
+        return (string)base.GetFront();
+    }
+
+    public string GetBack()
+    {
+        return (string)base.GetBack();
+    }
+
+    public bool Find(string val)
+    {
+        ListItem CurNode = beg;
+        while(CurNode != null)
+        {
+            if((string)CurNode.Data == val)
+                return true;
+            CurNode = CurNode.Next;
+        }
+        return false;
+    }
+}
---
+++ 2nd term/inisp_4(8)/main.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+class MainEntryPoint
+{
+    public static int Main()
+    {
+        Console.WriteLine("Lab №4, Ozierski Vital, group 052004");
+        
+        StringList sl = new StringList();
+        IntList il = new IntList();
+
+        string cmd, command, mode;
+        bool found;
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Commands: addback, addfront, getfront, getback, find, print");
+            Console.WriteLine("Format: <command> <int|string> or exit");
+            Console.Write("? ");
+            cmd = Console.ReadLine();
+            if (cmd == "exit")
+                break;
+            if (cmd.Split(' ').Length != 2)
+                Console.WriteLine("Incorrect command format");
+            else
+            {
+                command = cmd.Split(' ')[0];
+                mode = cmd.Split(' ')[1];
+                if (mode != "string" && mode != "int")
+                    Console.WriteLine("Incorrect mode");
+                else
+                {
+                    if (command == "addback")
+                    {
+                        Console.Write("Value: ");
+                        if (mode == "int")
+                            il.AddBack(Int32.Parse(Console.ReadLine()));
+                        else if (mode == "string")
+                            sl.AddBack(Console.ReadLine());
+                    }
+                    else if (command == "addfront")
+                    {
+                        Console.Write("Value: ");
+                        if (mode == "int")
+                            il.AddFront(Int32.Parse(Console.ReadLine()));
+                        else if (mode == "string")
+                            sl.AddFront(Console.ReadLine());
+                    }
+                    else if (command == "print")
+                    {
+                        if (mode == "int")
+                            il.Print();
+                        else if (mode == "string")
+                            sl.Print();
+                    }
+                    else if (command == "getfront")
+                    {
+                        if (mode == "int")
+                            Console.WriteLine(il.GetFront());
+                        else if (mode == "string")
+                            Console.WriteLine(sl.GetFront());
+                    }
+                    else if (command == "getback")
+                    {
+                        if (mode == "int")
+                            Console.WriteLine(il.GetBack());
+                        else if (mode == "string")
+                            Console.WriteLine(sl.GetBack());
+                    }
+                    else if (command == "find")
+                    {
+                        Console.Write("Value: ");
+                        found = false;
+                        if (mode == "int")
+                            found = il.Find(Int32.Parse(Console.ReadLine()));
+                        else if (mode == "string")
+                            found = sl.Find(Console.ReadLine());
+                        Console.WriteLine(found ? "found" : "no found");
+                    }
+                    else 
+                        Console.WriteLine("Incorrect Command");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        return 0;
+    }
+}
