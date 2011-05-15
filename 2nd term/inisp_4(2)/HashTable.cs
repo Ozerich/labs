ec819a9e48d48a990c7c5be1d7b1766d0dc0acdc
Ozerich <ozicoder@gmail.com>
2011-05-15 14:24:18 +0300

    Added inisp_4(2)

---
+++ 2nd term/inisp_4(2)/HashTable.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+class HashItem
+{
+    public object Data{get;set;}
+    public HashItem Next{get;set;}
+
+    public HashItem(object val)
+    {
+        Data = val;
+    }
+}
+
+abstract class HashTable
+{
+    protected HashItem[] Data;
+    protected int HashSize;
+
+    public HashTable(int H)
+    {
+        Data = new HashItem[H];
+        HashSize = H;
+    }
+
+    abstract public int Hash(object val);
+
+    public void Add(object val)
+    {
+        int key = Hash(val);
+        if (Data[key] == null)
+            Data[key] = new HashItem(val);
+        else
+        {
+            HashItem Cur = new HashItem(val);
+            Cur.Next = Data[key];
+            Data[key] = Cur;
+        }
+    }
+
+    public void Print()
+    {
+        HashItem Cur;
+        for (int i = 0; i < HashSize; i++)
+        {
+            Console.Write(i + " - ");
+            Cur = Data[i];
+            while (Cur != null)
+            {
+                Console.Write(Cur.Data + " ");
+                Cur = Cur.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+}
+
+class IntHashTable : HashTable
+{
+    public IntHashTable(int h)
+        : base(h)
+    {
+    }
+
+    public override int Hash(object val)
+    {
+        int key = (int)val;
+        return key % HashSize;
+    }
+
+    public void Add(int val)
+    {
+        base.Add((object)val);
+    }
+
+    public bool Find(int val)
+    {
+        int key = Hash(val);
+        HashItem Cur = Data[key];
+        while (Cur != null)
+        {
+            if ((int)Cur.Data == val)
+                return true;
+            Cur = Cur.Next;
+        }
+        return false;
+    }
+
+    public void Print()
+    {
+        HashItem Cur;
+        for (int i = 0; i < HashSize; i++)
+        {
+            Console.Write(i + " - ");
+            Cur = Data[i];
+            while (Cur != null)
+            {
+                Console.Write(Cur.Data + " ");
+                Cur = Cur.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class StringHashTable : HashTable
+{
+    public StringHashTable(int h)
+        : base(h)
+    {
+    }
+
+    public override int Hash(object val)
+    {
+        int key = ((string)val).Length;
+        return key % HashSize;
+    }
+
+    public void Add(string val)
+    {
+        base.Add((object)val);
+    }
+
+    public bool Find(string val)
+    {
+        int key = Hash(val);
+        HashItem Cur = Data[key];
+        while (Cur != null)
+        {
+            if ((string)Cur.Data == val)
+                return true;
+            Cur = Cur.Next;
+        }
+        return false;
+    }
+}
---
+++ 2nd term/inisp_4(2)/Main.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class MainEntryPoint
+{
+    public static int Main()
+    {
+        Console.WriteLine("Lab №4, Ozierski Vital, group 052004");
+
+        IntHashTable ih = new IntHashTable(50);
+        StringHashTable sh = new StringHashTable(16);
+
+        string cmd, command, mode;
+        bool found;
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Commands: add, find, print");
+            Console.WriteLine("Format: <command> <int|string> or exit");
+            Console.Write("? ");
+            cmd = Console.ReadLine();
+            if (cmd == "exit")
+                break;
+            if (cmd.Split(' ').Length != 2)
+                Console.WriteLine("Incorrect command format");
+            else
+            {
+                command = cmd.Split(' ')[0];
+                mode = cmd.Split(' ')[1];
+                if (mode != "string" && mode != "int")
+                    Console.WriteLine("Incorrect mode");
+                else
+                {
+                    if (command == "add")
+                    {
+                        Console.Write("Value: ");
+                        cmd = Console.ReadLine();
+                        if (mode == "string")
+                            sh.Add(cmd);
+                        else if (mode == "int")
+                            ih.Add(Int32.Parse(cmd));
+                    }
+                    else if (command == "find")
+                    {
+                        Console.Write("Value: ");
+                        cmd = Console.ReadLine();
+                        found = false;
+                        if (mode == "string")
+                            found = sh.Find(cmd);
+                        else if (mode == "int")
+                            found = ih.Find(Int32.Parse(cmd));
+                        Console.WriteLine(found ? "Found" : "No found");
+                    }
+                    else if (command == "print")
+                    {
+                        if (mode == "string")
+                            sh.Print();
+                        else if (mode == "int")
+                            ih.Print();
+                    }
+                    else
+                        Console.WriteLine("Incorrect Command");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        return 0;
+    }
+}
